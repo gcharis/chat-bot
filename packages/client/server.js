90e65a40f62e6996f18e40ea664bca7563a72495
gcharis <charis.giar@gmail.com>
2019-09-19 21:25:02 +0300

    client: Replace vanilla js with svelte

--- packages/client/server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import path from 'path';
-
-const app = express();
-
-app.use(express.static(path.resolve('./public')));
-
-app.listen(8000, () => console.log('http://localhost:8000'));
